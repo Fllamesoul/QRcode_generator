27c75e8c6d6faac18c025e7ebe4ed440e01d0c4b
Fllamesoul <flamesoul666@yandex.ru>
2022-01-27 10:50:56 +0300

    version 0.0.2

--- .idea/gtr.py
+++ .idea/gtr.py
@@ -26,6 +26,7 @@ path_doc = "QR_codes.docx"
 #   создает таблицу с заданным количеством столбоцв
 def create_doc_template(path_doc_file, num_rows, num_columns):
     doc_QR_document = Document()
+
     section = doc_QR_document.sections[0]
     section.page_height = Cm(29.7)      # высота листа в сантиметрах
     section.page_width = Cm(21.0)       # ширина листа в сантиметрах
@@ -53,26 +54,18 @@ def create_doc_template(path_doc_file, num_rows, num_columns):
 def make_QR_doc_list(object_excel, object_doc, list_num_col_for_QR: list,
                      list_num_col_QR_descr: list, path_doc: str):
 
-    doc_QR_document = Document()
+    doc_QR_document = Document("Template.docx")
 
 
     section = doc_QR_document.sections[0]
-    section.page_height = Cm(29.7)      # высота листа в сантиметрах
-    section.page_width = Cm(21.0)       # ширина листа в сантиметрах
-    section.left_margin = Mm(0)         # левое поле в миллиметрах
-    section.right_margin = Mm(0)        # правое поле в миллиметрах
-    section.top_margin = Mm(0)         # верхнее поле в миллиметрах
-    section.bottom_margin = Mm(0)      # нижнее поле в миллиметрах
-    section.header_distance = Mm(0)    # отступ от верхнего края страницы до
-    # нижнего края нижнего колонтитула
-    section.footer_distance = Mm(0)    # отступ от нижнего края страницы до
-    # нижнего края нижнего колонтитула
-
-
-    table = doc_QR_document.add_table(NUMBER_ROWS, NUMBER_COLS)
-    table.style ='Table Grid'
+    table = doc_QR_document.tables[0]
+    table._tbl.remove(table.rows[0]._tr)
     column_num_doc_table = 0
     row_cells = table.add_row().cells
+
+
+
+
 
     for row_num_doc_table in range(1, 25): #object_excel.rows + 1): FIXME
         print(row_num_doc_table) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
