6c47c3785690962035a107f16544c3b98e425766
Fllamesoul <flamesoul666@yandex.ru>
2022-01-31 08:13:02 +0300

    version 0.0.21

--- .idea/Main.py
+++ .idea/Main.py
@@ -1,3 +1,15 @@
-import gtr
 
-gtr._init_()
+import imp
+
+
+try:
+    fp, pathname, description = imp.find_module("gtr")
+    _mod = imp.load_module("gtr", fp, pathname, description)
+    _mod._init_()
+except Exception as exc:
+    print(exc)
+    input();
+
+
+    # компилировать в exe командой pyinstaller .idea/Main.py --hidden-import 'gtr'
+    # модуль gtr должен включать все библиотеки, используемые остальными модулями
--- .idea/gtr.py
+++ .idea/gtr.py
@@ -4,15 +4,19 @@ from docx import Document
 from docx.shared import Mm
 import os
 from docx.shared import Cm, Inches
-import excel
+import imp
+import openpyxl as xl
 
+
+fp, pathname, description = imp.find_module("excel")
+_mod = imp.load_module("excel", fp, pathname, description)
 
 PIC_FILE_NAME = "pic.png"
 NUMBER_COLS = 3
 NUMBER_ROWS = 0
 TABLE_CELL_WIDTH = Cm(5)
-WIDTH_QR = Cm(3)
-HEIGHT_QR = Cm(3)
+WIDTH_QR = Cm(1)
+HEIGHT_QR = Cm(1)
 
 path_excel_file = "Test table.xlsx"
 list_num_col_for_QR =   (1, 2, 3, 4, 6, 7, 8, 9)
@@ -26,7 +30,6 @@ path_doc = "QR_codes.docx"
 #   создает таблицу с заданным количеством столбоцв
 def create_doc_template(path_doc_file, num_rows, num_columns):
     doc_QR_document = Document()
-
     section = doc_QR_document.sections[0]
     section.page_height = Cm(29.7)      # высота листа в сантиметрах
     section.page_width = Cm(21.0)       # ширина листа в сантиметрах
@@ -39,13 +42,9 @@ def create_doc_template(path_doc_file, num_rows, num_columns):
     section.footer_distance = Mm(0)    # отступ от нижнего края страницы до
                                         # нижнего края нижнего колонтитула
 
-
-
-
     table = doc_QR_document.add_table(num_rows, num_columns, )
     table.style ='Table Grid'
     table.autofit = False
-
     return doc_QR_document
 
 
@@ -55,10 +54,9 @@ def make_QR_doc_list(object_excel, object_doc, list_num_col_for_QR: list,
                      list_num_col_QR_descr: list, path_doc: str):
 
     doc_QR_document = Document(path_doc)
-
-
     section = doc_QR_document.sections[0]
     table = doc_QR_document.tables[0]
+
     table._tbl.remove(table.rows[0]._tr)
     column_num_doc_table = 0
     row_cells = table.add_row().cells
@@ -67,11 +65,15 @@ def make_QR_doc_list(object_excel, object_doc, list_num_col_for_QR: list,
         print(row_num_doc_table) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         make_img_QR(object_excel, row_num_doc_table + 1, list_num_col_for_QR)
         paragraph = row_cells[column_num_doc_table].paragraphs[0]
+        paragraph.alignment = 1
         run = paragraph.add_run()
         row_cells[column_num_doc_table].add_paragraph().add_run().\
             add_text(get_string_from_excel(object_excel, row_num_doc_table + 1, list_num_col_QR_descr))
+        row_cells[column_num_doc_table].paragraphs[1].alignment = 1
+
         if row_num_doc_table % 3 == 0 and row_num_doc_table != object_excel.rows - 1:
             row_cells = table.add_row().cells       # new string
+
         run.add_picture(PIC_FILE_NAME, width = WIDTH_QR, height = HEIGHT_QR)
         column_num_doc_table = row_num_doc_table % 3
 
@@ -81,7 +83,7 @@ def make_QR_doc_list(object_excel, object_doc, list_num_col_for_QR: list,
     doc_QR_document.save(object_doc)
 
 
-def get_string_from_excel(excel_file: excel.Excel, num_row: int, list_num_col: list):
+def get_string_from_excel(excel_file: _mod.Excel, num_row: int, list_num_col: list):
     data = ""
     for i in list_num_col:
         data += excel_file.get_data_cell(num_row, i)
@@ -95,9 +97,15 @@ def make_img_QR(excel_file, num_row, list_num_col_for_QR):
 
 
 def _init_():
-    path_excel_file = input("Open excel file: ")
-    ex = excel.Excel(path_excel_file)
-    path_template_docx_file = input("Open template docx file: (Template.docx by default)")
+    fp, pathname, description = imp.find_module("excel")
+    _mod = imp.load_module("excel", fp, pathname, description)
+    #path_excel_file = input("Open excel file: (Для инвентаризации.xlsx by default)")
+    path_excel_file = "Для инвентаризации.xlsx" #FIXME
+    if (path_excel_file == ""):
+        path_excel_file = "Для инвентаризации.xlsx"
+    ex = _mod.Excel(path_excel_file)
+    #path_template_docx_file = input("Open template docx file: (Template.docx by default)")
+    path_template_docx_file = "Template.docx" #FIXME
     if (path_template_docx_file == ""):
         path_template_docx_file = "Template.docx"
     make_QR_doc_list(ex, "result.docx", list_num_col_for_QR, list_num_col_QR_descr, path_template_docx_file)
